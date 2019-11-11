162ae0ff7e9e85187e8c6084aaaeed0df5f945bd
matt manzo <mmanzo@gmail.com>
2019-11-11 14:16:16 -0500

    react setup

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "itc6480-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://0wswag2e6j.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_IdRtUtshE",
+      APP_CLIENT_ID: "48emebe8o02lq79ektpb07vnqr",
+      IDENTITY_POOL_ID: "us-east-1:dc08904a-735e-4008-9b81-df6178be1bcc"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>ITC6480</h1>
+        <p>rm Servers</p>
+      </div>
+    </div>
+  );
+}

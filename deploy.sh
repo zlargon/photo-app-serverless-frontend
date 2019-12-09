ba2ceba757d651a3bab650bf4c731d8b7107d929
zlargon <zlargon1988@gmail.com>
2019-12-09 12:43:01 -0500

    [deploy] build react app and upload website to s3 bucket

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+cd ${0%/*}
+function echo_y() { echo -e "\033[1;33m$@\033[0m" ; } # yellow echo
+
+# The S3 bucket for static website
+S3_REACT_APP_BUCKET=""
+
+# 1. build react app
+npm run build
+
+# 2. upload build folder
+echo_y "Upload static website to s3 bucket"
+cd build
+aws s3 sync . "s3://${S3_REACT_APP_BUCKET}"
+
+# 3. open URL
+open "http://${S3_REACT_APP_BUCKET}.s3-website-us-east-1.amazonaws.com"
--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   STRIPE_KEY: "pk_test_SHMgaMI6U1lfn5i05oMxKTVM00ne6RLkT9",
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "itc6480-uploads"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://0wswag2e6j.execute-api.us-east-1.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_dP7RKg4kv",
-      APP_CLIENT_ID: "413h5cpi2oaou1ivlfakc26qnh",
-      IDENTITY_POOL_ID: "us-east-1:dc08904a-735e-4008-9b81-df6178be1bcc"
-    }
-  };
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "itc6480-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://0wswag2e6j.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_dP7RKg4kv",
+    APP_CLIENT_ID: "413h5cpi2oaou1ivlfakc26qnh",
+    IDENTITY_POOL_ID: "us-east-1:dc08904a-735e-4008-9b81-df6178be1bcc"
+  }
+};
